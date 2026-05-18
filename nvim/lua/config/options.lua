670693777323590852cdda013d49b0ea6831af7e
YOUR_NAME <YOUR_EMAIL@example.com>
2026-05-18 15:59:15 +0800

    chore: update clipboard and shell config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Force clipboard yanks over SSH/tmux to use OSC 52.
+vim.g.clipboard = "osc52"
+vim.opt.clipboard = "unnamedplus"
